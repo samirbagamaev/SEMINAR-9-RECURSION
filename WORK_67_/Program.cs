1d4ac8e751e5d69abe59dd1b08d540dfa0607816
Samir Bagamaev <05ru4315@gmail.com>
2023-01-30 21:59:05 +0300

    WORK_67

---
+++ WORK_67_/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+const int numberSum = 453;
+
+int SumNumber (int N)
+{
+    int sum = 0;
+    if (N == 0) return 0;
+
+    else
+    {
+        sum = N % 10 + SumNumber(N / 10);
+    }
+    return sum;
+}
+
+Console.WriteLine($"РЕЗУЛЬТАТ: {SumNumber(numberSum)}");
